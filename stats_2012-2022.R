cc940de36e47a0dd3182cea5c09358e05afd57bb
Anna Fiesinger <123569008+afiesinger@users.noreply.github.com>
2025-01-17 11:17:38 +0100

    Update stats_2012-2022.R

--- stats_2012-2022.R
+++ stats_2012-2022.R
@@ -1,4 +1,5 @@
 #!/usr/bin/env Rscript
 
 # statistical testing of decadal community composition changes
-#' -----------------------------------------------------------
+# datasets used: May 2022 (Fiesinger et al.) and 2012-2014 (Howells et al. 2020)
+#' -----------------------------------------------------------------------------
