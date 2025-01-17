7f5cc37a4803f4b684d911613aae05553a73ac7b
Anna Fiesinger <123569008+afiesinger@users.noreply.github.com>
2025-01-17 11:17:04 +0100

    Update stats_2022.R

--- stats_2022.R
+++ stats_2022.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env Rscript
 
 # statistical analyses on ITS2 post-med sequences and ITS2 type profiles output by SymPortal
+# tests done on the complete dataset from May 2022 collected by Fiesinger et al.
 #' -----------------------------------------------------------------------------------------
 
